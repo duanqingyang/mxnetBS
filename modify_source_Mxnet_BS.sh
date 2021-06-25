ecdea929c93f13198f63957e094d7a98868bf90f
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2021-06-26 00:10:14 +0800

    Update and rename modify_Mxnet_BS.sh to modify_source_Mxnet_BS.sh

--- modify_source_Mxnet_BS.sh
+++ modify_source_Mxnet_BS.sh
@@ -1,3 +1,14 @@
+install mxnet from source: 
+1) pre-requisite: 
+     sudo apt install libopencv-dev
+      sudo apt-get install libatlas-base-dev
+2) cd mxnet/
+3) make
+4) python -m pip install --user -e ./python  
+#Note that the -e flag is optional. It is equivalent to --editable 
+#and means that if you edit the source files, these changes will be reflected in the package installed
+
+
 modify BS:
 1) change BS python code in ~/Bytescheduler
 2) sudo python setup.py install --user   # will update BS lib python in ~/.local/lib/python2.7
