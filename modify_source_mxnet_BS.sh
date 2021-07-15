05b9638bf36de3a301c67d2e133186d76ff43b4b
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2021-07-15 16:05:35 +0800

    Update and rename modify_source_Mxnet_BS.sh to modify_source_mxnet_BS.sh

--- modify_source_mxnet_BS.sh
+++ modify_source_mxnet_BS.sh
@@ -1,3 +1,8 @@
+mxnet: 1.5.0 (cuda: 9.0)
+BS: 
+gluonnlp: 0.8.3
+
+
 1) install mxnet from pip
  pip install --user mxnet-cu90==1.5.0 # will appear at ~/.local/lib/python2.7/site-packages/mxnet
 
@@ -10,28 +15,35 @@
       sudo ldconfig
  cd mxnet/
  make
- python -m pip install --user -e ./python  
+ sudo python -m pip install --user -e ./python  
 #Note that the -e flag is optional. It is equivalent to --editable 
 #and means that if you edit the source files, these changes will be reflected in the package installed
+# --user means installing to your own account python lib space
 
-3) modify Mxnet python lib:
+3) install gluonnlp from source:
+ cd gluonnlp
+ sudo pip install --user -e .
+ (or sudo python setup.py install --user)
+
+4) modify Mxnet python lib:
      modify Mxnet python code in ~/incubator-mxnet/python
      # mxnet python lib in ~/.local/lib/python2.7  is link to  ~/incubator-mxnet/python. So dont need to do python install again
 
-4) modify Mxnet c++ lib:
+5) modify Mxnet c++ lib:
      modify Mxnet c++ code in ~/incubator-mxnet/src
      make again so to update libmxnet.so in ~/incubator-mxnet/lib, which is used by mxnet python lib in ~/incubator-mxnet/python
 
-5) install BS
+6) install BS from source
   pre-requisite: 
     sudo pip install bayesian-optimization==1.0.1 six
   cd bytescheduler/
   vim setup.py to add MXNET_ROOT path
-  sudo python setup.py install  --user 
+  sudo pip install --user -e .
+  (or sudo python setup.py install --user)
   One problem: cannot find **.so, this is because LD_LIBRARY_PATH in sudo is dofferent
        we need to add lib path to /etc/ld.so.conf then run sudo ldconfig
 
-6) modify BS:
+7) modify BS:
   python -m pip uninstall bytescheduler #uninstall
   change BS python code in ~/Bytescheduler
   sudo python setup.py install --user   # will update BS lib python in ~/.local/lib/python2.7
